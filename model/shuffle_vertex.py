e28ff877e4864fe3dcf0c3d7326c7896ea933798
tunm <tunmxy@163.com>
2022-11-09 20:30:39 +0800

    测试增强

--- model/shuffle_vertex.py
+++ model/shuffle_vertex.py
@@ -1,26 +1,30 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
-from .backbone import ShuffleNetV2
+from backbone import ShuffleNetV2
 
 
 class ShuffleVertex(nn.Module):
 
     def __init__(self, model_size='1.5x', pretrain=True, with_last_conv=False):
         super().__init__()
-        self.backbone = ShuffleNetV2(model_size='1.5x', pretrain=True, with_last_conv=True)
-        self.conv1 = nn.Conv2d(1024, 128, 1, 1, 0, bias=False)
+        self.backbone = ShuffleNetV2(model_size='1.5x', pretrain=True, with_last_conv=False)
         self.pool1 = nn.AvgPool2d(4, stride=1)
-        self.linear = nn.Linear(128, 8)
+        self.prelu1 = nn.PReLU()
+        self.linear = nn.Linear(704, 256)
+        self.drop = nn.Dropout(0.25)
         self.relu6 = nn.ReLU6()
+        self.kps = nn.Linear(256, 8)
 
     def forward(self, x):
         x = self.backbone(x)
-        x = self.conv1(x)
         x = self.pool1(x)
         x = x.view(x.size(0), -1)
         x = self.linear(x)
+        x = self.drop(x)
         x = self.relu6(x)
+        x = self.kps(x)
+
         return x
 
 
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 from data import VertexDataset
 from model.shuffle_vertex import ShuffleVertex
 from torch.utils.data import DataLoader
@@ -5,7 +8,7 @@ import torch
 
 device = torch.device("cpu")
 net = ShuffleVertex()
-net.load_state_dict(torch.load("save_dir/best_model.pth", map_location="cpu"))
+net.load_state_dict(torch.load("save_dir/last.pth", map_location="cpu"))
 net.to(device)
 
 batch_size = 1
@@ -20,3 +23,21 @@ y = net(img_tensor)
 
 print(label_tensor)
 print(y)
+
+image_decode = img_tensor[0].numpy() * 255
+image_decode = image_decode.transpose(1, 2, 0)
+image_decode = image_decode.astype(np.uint8)
+image_decode = image_decode.copy()
+h, w, _ = image_decode.shape
+print(image_decode.shape)
+
+kps = y.detach().numpy().reshape(4, 2)
+kps[:, 0] *= w
+kps[:, 1] *= h
+
+for x, y in kps.astype(np.int32):
+    print(x, y)
+    cv2.line(image_decode, (x, y), (x, y), (100, 100, 255), 3)
+
+cv2.imshow("w", image_decode)
+cv2.waitKey(0)
