8a4d19abead004b49ff17b6a38fe208e289a6310
tunm <tunmxy@163.com>
2022-11-09 22:22:45 +0800

    测试增强

--- model/shuffle_vertex.py
+++ model/shuffle_vertex.py
@@ -8,12 +8,12 @@ class ShuffleVertex(nn.Module):
 
     def __init__(self, model_size='1.5x', pretrain=True, with_last_conv=False):
         super().__init__()
-        self.backbone = ShuffleNetV2(model_size='1.5x', pretrain=True, with_last_conv=False)
+        self.backbone = ShuffleNetV2(model_size=model_size, pretrain=pretrain, with_last_conv=with_last_conv)
         self.pool1 = nn.AvgPool2d(4, stride=1)
-        self.prelu1 = nn.PReLU()
         self.linear = nn.Linear(704, 256)
         self.drop = nn.Dropout(0.25)
-        self.relu6 = nn.ReLU6()
+        # self.relu6 = nn.ReLU6()
+        self.prelu1 = nn.PReLU()
         self.kps = nn.Linear(256, 8)
 
     def forward(self, x):
@@ -22,7 +22,7 @@ class ShuffleVertex(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.linear(x)
         x = self.drop(x)
-        x = self.relu6(x)
+        x = self.prelu1(x)
         x = self.kps(x)
 
         return x
--- test.py
+++ test.py
@@ -11,7 +11,7 @@ net = ShuffleVertex()
 net.load_state_dict(torch.load("save_dir/last.pth", map_location="cpu"))
 net.to(device)
 
-batch_size = 1
+batch_size = 128
 val_dir = "oinbagCrawler_vertex_train/val"
 val_dataset = VertexDataset(val_dir, mode='val', is_show=False)
 val_dataloader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=True)
@@ -19,25 +19,32 @@ val_dataloader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=
 img_tensor, label_tensor = next(iter(val_dataloader))
 
 net.eval()
-y = net(img_tensor)
+with torch.no_grad():
+    y_hat = net(img_tensor)
 
 print(label_tensor)
-print(y)
+print(y_hat)
 
-image_decode = img_tensor[0].numpy() * 255
-image_decode = image_decode.transpose(1, 2, 0)
-image_decode = image_decode.astype(np.uint8)
-image_decode = image_decode.copy()
-h, w, _ = image_decode.shape
-print(image_decode.shape)
+y_hat = y_hat.detach().numpy()
 
-kps = y.detach().numpy().reshape(4, 2)
-kps[:, 0] *= w
-kps[:, 1] *= h
+for idx in range(batch_size):
+    image_decode = img_tensor[idx].numpy() * 255
+    image_decode = image_decode.transpose(1, 2, 0)
+    image_decode = image_decode.astype(np.uint8)
+    image_decode = image_decode.copy()
+    h, w, _ = image_decode.shape
+    print(image_decode.shape)
+    print(label_tensor[idx].numpy())
+    print(y_hat[idx])
 
-for x, y in kps.astype(np.int32):
-    print(x, y)
-    cv2.line(image_decode, (x, y), (x, y), (100, 100, 255), 3)
 
-cv2.imshow("w", image_decode)
-cv2.waitKey(0)
+    kps = y_hat[idx].reshape(4, 2)
+    kps[:, 0] *= w
+    kps[:, 1] *= h
+
+    for x, y in kps.astype(np.int32):
+        # print(x, y)
+        cv2.line(image_decode, (x, y), (x, y), (100, 100, 255), 3)
+
+    cv2.imshow("w", image_decode)
+    cv2.waitKey(0)
