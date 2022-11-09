fbdcfe1491dad4ce88407fc618996f76ff7e0aba
tunm <tunmxy@163.com>
2022-11-09 23:45:33 +0800

    测试增强

--- test.py
+++ test.py
@@ -37,14 +37,38 @@ for idx in range(batch_size):
     print(label_tensor[idx].numpy())
     print(y_hat[idx])
 
-
     kps = y_hat[idx].reshape(4, 2)
     kps[:, 0] *= w
     kps[:, 1] *= h
+
+    pts = np.float32([[0, 0], [w * 2, 0], [w * 2, h], [0, h], ])
+    matrix = cv2.getPerspectiveTransform(kps, pts)
+    output = cv2.warpPerspective(image_decode, matrix, (w * 2, h))
+
+
 
     for x, y in kps.astype(np.int32):
         # print(x, y)
         cv2.line(image_decode, (x, y), (x, y), (100, 100, 255), 3)
 
+
+
+    x2 = cv2.resize(output, (112, 112))
+    data2 = x2 / 255.0
+    data2 = data2.transpose(2, 0, 1)
+    data2 = np.expand_dims(data2, 0)
+    data2 = torch.Tensor(data2)
+    kps2 = net(data2).detach().numpy()
+    kps2 = kps2.reshape(4, 2)
+    kps2[:, 0] *= w
+    kps2[:, 1] *= h
+
+
+    for x, y in kps2.astype(np.int32):
+        # print(x, y)
+        cv2.line(x2, (x, y), (x, y), (100, 100, 255), 3)
+
+    cv2.imshow("q", output)
     cv2.imshow("w", image_decode)
+    cv2.imshow("x2", x2)
     cv2.waitKey(0)
