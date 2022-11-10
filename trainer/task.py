421fa6adceeecc7abff23fc86816a1018b1ad1ba
tunmx <tunmxy@163.com>
2022-11-10 10:02:23 +0800

    添加net

--- trainer/task.py
+++ trainer/task.py
@@ -99,7 +99,7 @@ class TrainTask(object):
         val_loss = 0.0
         # val_acc = 0.0
         with torch.no_grad():
-            val_bar = tqdm(val_data')
+            val_bar = tqdm(val_data)
             for step, data in enumerate(val_bar):
                 val_images, val_labels = data
                 # val_images[0] = np.
