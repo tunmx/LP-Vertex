3ca118cb833bd14d30b9fcbdc1ef3cb850d45bd1
tunmx <tunmxy@163.com>
2022-11-10 12:06:32 +0800

    添加net

--- data/transform.py
+++ data/transform.py
@@ -38,7 +38,7 @@ class Pipeline(object):
                 mode="constant"  # 定义填充图像外区域的方法
             )),
 
-            iaa.Resize((96, 96), ),
+            iaa.Resize((112, 112), ),
         ])
 
         self.seq_map = dict(train=self.train_seq, val=self.val_seq)
