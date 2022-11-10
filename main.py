edb0eeafb07eb9404c2be8f6abc7f6b11b535aa2
tunmx <tunmxy@163.com>
2022-11-10 17:08:43 +0800

    添加net

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ logger.info(f"Verification Dataset Total: {len(val_dataset)}")
 net = ShuffleVertex()
 
 lr_schedule_option = dict(name='ExponentialLR', gamma=0.1)
-optimizer_option = dict(name='SGD', lr=0.001, momentum=0.5, weight_decay=0.0005)
+optimizer_option = dict(name='SGD', lr=0.01, momentum=0.5, weight_decay=0.0005)
 
 task_option = dict(model=net, save_dir='save_dir_t1', loss_func='mse_loss', lr_schedule_option=lr_schedule_option,
                    optimizer_option=optimizer_option, weight_path=None)
