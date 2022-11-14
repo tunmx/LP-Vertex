f9d8314733caed01c40c4fe36778d4eac7dcd86f
tunm <tunmxy@163.com>
2022-11-14 23:30:50 +0800

    测试增强

--- train_mnetv2.py
+++ train_mnetv2.py
@@ -20,7 +20,7 @@ logger.info(f"Verification Dataset Total: {len(val_dataset)}")
 
 net = MobileVertex()
 
-lr_schedule_option = dict(name='ExponentialLR', gamma=0.1)
+lr_schedule_option = dict(name='StepLR', gamma=0.1, step_size=10)
 optimizer_option = dict(name='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)
 
 task_option = dict(model=net, save_dir='save_dir_t2', loss_func='mse_loss', lr_schedule_option=lr_schedule_option,
