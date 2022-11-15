f0b332b3fb9a8003996b52db625d7be15094cfb6
tunm <tunmxy@163.com>
2022-11-15 22:43:57 +0800

    测试增强

--- model/backbone/__init__.py
+++ model/backbone/__init__.py
@@ -1,4 +1,4 @@
 from .shufflenetv2 import ShuffleNetV2
 from .mobilenetv2 import MobileNetV2
-from .mobilenetv2p import MobileNetV2p
+from .resnet import ResNet
 
--- model/resnet_vertex.py
+++ model/resnet_vertex.py
@@ -1,26 +1,28 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
-from .backbone import MobileNetV2p
+from backbone import ResNet
 
 
-class MobileVertex(nn.Module):
+class ResNetVertex(nn.Module):
 
-    def __init__(self, width_mult=1., ):
+    def __init__(self, depth=50):
         super().__init__()
-        self.backbone = MobileNetV2p(width_mult=width_mult)
+        self.backbone = ResNet(depth)
         self.pool1 = nn.AvgPool2d(4, stride=1)
-        self.fc = nn.Linear(1280, 128)
+        self.fc1 = nn.Linear(2048, 256)
+        self.fc2 = nn.Linear(256, 128)
         # self.relu6 = nn.ReLU6()
         self.prelu1 = nn.PReLU()
         self.kps = nn.Linear(128, 8)
 
     def forward(self, x):
         x = self.backbone(x)
-        print(x.shape)
+        # print(x.shape)
         x = self.pool1(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
         x = self.prelu1(x)
         x = self.kps(x)
 
@@ -28,8 +30,8 @@ class MobileVertex(nn.Module):
 
 
 if __name__ == '__main__':
-    net = MobileVertex()
-    # print(net)
+    net = ResNetVertex()
+    print(net)
 
     x = torch.rand(1, 3, 112, 112)
     # print(x.shape)
