4f83412cc260380461bb2670652c0d43a69d5d2a
tunm <tunmxy@163.com>
2022-12-14 20:55:03 +0800

    up

--- breezevertex/command/make_dataset.py
+++ breezevertex/command/make_dataset.py
@@ -4,10 +4,32 @@ import shutil
 from loguru import logger
 import tqdm
 import click
+import json
 
 from breezevertex.data import LabelMeDataset
 
 __all__ = ['make']
+
+CODE_OK = 0  # 通过
+CODE_NOT_FOUND = 1  # 缺少标注文件
+CODE_NOT_LABELED = 2  # 没有标注数据
+CODE_NOT_MATCH = 3  # 标注数据不匹配
+
+
+def check(img_path, label_path):
+    if not os.path.exists(label_path):
+        # logger.info(f"{label_path} 标注文件不存在!")
+        return CODE_NOT_FOUND
+    with open(label_path, 'r') as f:
+        data = json.load(f)
+    if not data['shapes']:
+        # logger.info(f"{label_path} 缺少标注数据!")
+        return CODE_NOT_LABELED
+    # print(len(data['shapes'][0]['points']))
+    if len(data['shapes'][0]['points']) != 4:
+        # logger.info(f"{label_path} 数据标注不匹配!")
+        return CODE_NOT_MATCH
+    return CODE_OK
 
 
 @click.command(help="Create a data set for training.")
@@ -36,6 +58,8 @@ def make(path, save_path, split_rate, suffix):
         label_path = "".join(img_path.split(".")[:-1]) + f".{suffix}"
         dst_img = os.path.join(train_dir, os.path.basename(img_path))
         dst_label = os.path.join(train_dir, os.path.basename(label_path))
+        if check(dst_img, dst_label) == CODE_OK:
+            continue
         shutil.copy(img_path, dst_img)
         shutil.copy(label_path, dst_label)
 
@@ -44,6 +68,8 @@ def make(path, save_path, split_rate, suffix):
         label_path = "".join(img_path.split(".")[:-1]) + f".{suffix}"
         dst_img = os.path.join(val_dir, os.path.basename(img_path))
         dst_label = os.path.join(val_dir, os.path.basename(label_path))
+        if check(dst_img, dst_label) == CODE_OK:
+            continue
         shutil.copy(img_path, dst_img)
         shutil.copy(label_path, dst_label)
 
